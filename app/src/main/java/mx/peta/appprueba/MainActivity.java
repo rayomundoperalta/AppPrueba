92e62c2f59b3485bc23c02b4296ee305a0336d4b
Raymundo Peralta <raymundo_peralta@yahoo.com>
2016-06-01 18:38:15 -0500

    ejemplo capitulo 16

--- app/src/main/java/mx/peta/appprueba/MainActivity.java
+++ app/src/main/java/mx/peta/appprueba/MainActivity.java
@@ -1,13 +1,20 @@
 package mx.peta.appprueba;
 
 import android.support.v7.app.AppCompatActivity;
+import android.databinding.DataBindingUtil;
 import android.os.Bundle;
+
+import mx.peta.appprueba.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        String textFieldValue ="";
+        ActivityMainBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+        MainActivityHandlers handler = new MainActivityHandlers();
+        binding.setHandler(handler);
+
     }
 }
