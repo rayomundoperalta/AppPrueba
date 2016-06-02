49b200031d31250d3a7229987f4ebc540eb05e65
Raymundo Peralta <raymundo_peralta@yahoo.com>
2016-06-01 19:38:21 -0500

    ejemplo capitulo 17 con reloj

--- app/src/main/java/mx/peta/appprueba/MainActivity.java
+++ app/src/main/java/mx/peta/appprueba/MainActivity.java
@@ -3,6 +3,9 @@ package mx.peta.appprueba;
 import android.support.v7.app.AppCompatActivity;
 import android.databinding.DataBindingUtil;
 import android.os.Bundle;
+import android.widget.LinearLayout;
+import android.widget.RadioGroup;
+import android.widget.TextClock;
 
 import mx.peta.appprueba.databinding.ActivityMainBinding;
 
@@ -15,6 +18,21 @@ public class MainActivity extends AppCompatActivity {
         ActivityMainBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
         MainActivityHandlers handler = new MainActivityHandlers();
         binding.setHandler(handler);
+        TextClock textClock;
+        textClock = (TextClock)findViewById(R.id.textClock);
 
+        final RadioGroup group1 = (RadioGroup) findViewById(R.id.orientation);
+        group1.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                switch (checkedId) {
+                    case R.id.horizontal:
+                        group.setOrientation(LinearLayout.HORIZONTAL);
+                        break;
+                    case R.id.vertical:
+                        group.setOrientation(LinearLayout.VERTICAL);
+                        break;
+                }
+            }
+        });
     }
 }
